d185bbf265553b67f8367e9fe0c6bda04f203cb6
Integralist <mark.mcdx@gmail.com>
2014-01-17 17:11:15 +0000

    Fix broken test

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'RSpec Greeter' do
-  it 'should say 'Hello RSpec!' when it receives the greet() message' do
+  it 'should say "Hello RSpec!" when it receives the greet() message' do
     greeter  = RSpecGreeter.new         # Given
     greeting = greeter.greet            # When
     greeting.should eq('Hello RSpec!')  # Then
