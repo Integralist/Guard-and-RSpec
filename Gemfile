f457c76085f2c27ec1f42e963234c065a4761c88
Integralist <mark.mcdx@gmail.com>
2014-01-19 15:05:39 +0000

    Add debugging details to the repo

--- Gemfile
+++ Gemfile
@@ -6,4 +6,9 @@ group :development do
   gem 'guard'
   gem 'guard-rspec'
   gem 'pry'
+
+  # Adds debugging steps to Pry
+  # continue, step, next
+  gem 'pry-remote'
+  gem 'pry-nav'
 end
--- lib/example.rb
+++ lib/example.rb
@@ -1,7 +1,24 @@
 $: << File.dirname(__FILE__)
 
 class RSpecGreeter
+  attr_accessor :test
+
+  @@class_property = "I'm a class property"
+
   def greet
+    binding.pry
+    @instance_property = "I'm an instance property"
     "Hello RSpec!"
+  end
+
+  def pubs
+    test_var = "I'm a test variable"
+    test_var
+  end
+
+  private
+
+  def privs
+    puts "I'm private"
   end
 end
