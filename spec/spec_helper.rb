d15b6f3c4611d39eea78846bf5aa3820b161cbf7
Integralist <mark.mcdx@gmail.com>
2014-01-26 20:40:43 +0000

    only load Pry in debug mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 $: << File.join(File.dirname(__FILE__), '..', 'lib')
 
-require 'pry' # add `binding.pry` wherever you need to debug
+require 'pry' if ENV['APP_ENV'] == 'debug' # add `binding.pry` wherever you need to debug
 require 'example'
